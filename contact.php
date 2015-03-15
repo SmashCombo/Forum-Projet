80bc49a47aeaa46e1012e2f6ef9aee010a8238e8
Paul <paul.moyse@live.fr>
2015-03-15 22:18:03 +0000

    fix 1.0

--- contact.php
+++ contact.php
@@ -82,12 +82,19 @@
             <h2>Contact Us</h2>
             <p>This is what a contact form could look like on this website template:</p>
             <div style="width:120px; float:left;"><p>Name</p></div>
-			<div style="width:430px; float:right;"><p><input class="contact" type="text" name="your_name" value="" /></p></div>
-            <div style="width:120px; float:left;"><p>Email Address</p></div>
-			<div style="width:430px; float:right;"><p><input class="contact" type="text" name="your_email" value="" /></p></div>
-            <div style="width:120px; float:left;"><p>Message</p></div>
+          <p>&nbsp;</p>
+          <div style="width:430px; float:right;"><p><input class="contact" type="text" name="your_name" value="" /></p>
+            <p>&nbsp;</p>
+          </div>
+          <div style="width:120px; float:left;">
+          <p>	Email Address</p>
+          </div>
+          <div style="width:430px; float:right;"><p><input class="contact" type="text" name="your_email" value="" /></p></div>
+            <div style="width:120px; float:left;"><p>Message</p>
+            </div>
 			<div style="width:430px; float:right;"><p><textarea class="contact textarea" rows="8" cols="50" name="your_message"></textarea></p></div>
-            <br style="clear:both;" />
+			<p><br style="clear:both;" />
+            </p>
             <p style="padding: 10px 0 10px 0;">Please enter the answer to this simple maths question (to prevent spam)</p>
             <div style="width:120px; float:left;"><p>Maths Question: 9 + 3 = ?</p></div>
 			<div style="width:430px; float:right;"><p><input type="text" name="user_answer" class="contact" /><input type="hidden" name="answer" value="4d76fe9775" /></p></div>
